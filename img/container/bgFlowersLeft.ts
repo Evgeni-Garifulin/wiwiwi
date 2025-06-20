f37f5f98276d812e4b3faa47a63bcafcf26c3bb2
Evgeni-Garifulin <melegarifulin@gmail.com>
2025-06-20 22:49:15 +0300

    deploy

--- img/container/bgFlowersLeft.ts
+++ img/container/bgFlowersLeft.ts
@@ -1,9 +1,9 @@
 const bgFlowersLeft = {
-	img1x: '/img/container/bg-flowers-left@1x.png',
-	webp1x: '/img/container/bg-flowers-left@1x.webp',
-	avif1x: '/img/container/bg-flowers-left@1x.avif',
-	img2x: '/img/container/bg-flowers-left@2x.png',
-	webp2x: '/img/container/bg-flowers-left@2x.webp',
-	avif2x: '/img/container/bg-flowers-left@2x.avif',
+	img1x: './img/container/bg-flowers-left@1x.png',
+	webp1x: './img/container/bg-flowers-left@1x.webp',
+	avif1x: './img/container/bg-flowers-left@1x.avif',
+	img2x: './img/container/bg-flowers-left@2x.png',
+	webp2x: './img/container/bg-flowers-left@2x.webp',
+	avif2x: './img/container/bg-flowers-left@2x.avif',
 };
 export default bgFlowersLeft;
